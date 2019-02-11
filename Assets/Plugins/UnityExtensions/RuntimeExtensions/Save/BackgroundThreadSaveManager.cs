bb84c97a3ec1823f5fd69d2382651164485e3f6d
yuyang <yuyang9119@outlook.com>
2019-02-12 07:32:36 +0800

    添加存档系统（已实现 Standalone）

---
+++ Assets/Plugins/UnityExtensions/RuntimeExtensions/Save/BackgroundThreadSaveManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+
+namespace UnityExtensions
+{
+    /// <summary>
+    /// 背景线程存档管理
+    /// </summary>
+    public class BackgroundThreadSaveManager : SaveManager
+    {
+        AutoResetEvent _event;
+        Thread _thread;
+        SaveTask _task;
+
+
+        public BackgroundThreadSaveManager()
+        {
+            _event = new AutoResetEvent(false);
+
+            _thread = new Thread(BackgroundThread)
+            {
+                IsBackground = true,
+                Priority = ThreadPriority.Lowest,
+                Name = "Background Thread",
+            };
+
+            _thread.Start();
+        }
+
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _event.Close();
+            _thread.Abort();
+        }
+        
+
+        protected override void BeginTask(SaveTask task)
+        {
+            _task = task;
+            _event.Set();
+        }
+
+
+        protected override void FinishTask(SaveTask task)
+        {
+        }
+
+
+        // 后台线程
+        void BackgroundThread()
+        {
+            while (true)
+            {
+                _event.WaitOne();
+
+                switch (_task.type)
+                {
+                    case SaveTask.Type.Load:
+                        try
+                        {
+                            _task.data = _task.target.Read();
+                        }
+                        catch (Exception e)
+                        {
+                            _task.exception = e;
+                        }
+                        break;
+
+                    case SaveTask.Type.Save:
+                        if (_task.success)
+                        {
+                            try
+                            {
+                                _task.target.Write(_task.data);
+                            }
+                            catch (Exception e)
+                            {
+                                _task.exception = e;
+                            }
+                        }
+                        break;
+
+                    case SaveTask.Type.Delete:
+                        try
+                        {
+                            _task.target.Delete();
+                        }
+                        catch (Exception e)
+                        {
+                            _task.exception = e;
+                        }
+                        break;
+                }
+
+                _task = null;
+                _finished = true;
+            }
+        }
+
+    } // class BackgroundThreadSaveManager
+
+} // namespace UnityExtensions
